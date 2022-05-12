92cad95c5d320c0fa99cffd3d1b479b961f727d2
Letalandroid <carlossoncra@protonmail.com>
2022-05-12 08:52:30 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const convert = document.querySelector("#convert"),
+      pintarApi = document.querySelector(".result"),
+      url = document.querySelector("#link");
+
+const css = "#f00";
+
+convert.addEventListener("click", () => {
+
+    pintarApi.innerHTML = "";
+
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("style", "width:800px;height:250px;border:0;overflow:hidden;margin-top:20px");
+    iframe.setAttribute("scrolling", "no");
+    iframe.setAttribute("src", `https://loader.to/api/card/?url=${url.value}`);
+
+    pintarApi.insertAdjacentElement("beforeend", iframe);
+
+})
